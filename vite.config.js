53f9e6b7bc3ee74f3fd45e17ee46d5085dfd93bb
shaopengbian <shaopengbian@microsoft.com>
2022-08-11 14:46:05 +0800

    Remove useless code

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,7 @@
-// // vite.config.js
-// import { defineConfig } from 'vite'
-// import dns from 'dns'
-
-
-
-// dns.setDefaultResultOrder('verbatim')
-
-// export default defineConfig({
-//   // omit
-
-//   plugins: [
-//     basicSsl()
-//   ] ,
-
-//   server: {
-//     open: '/index.html'
-//   }
-// })
-
-
 import mkcert from 'vite-plugin-mkcert'
 export default {
     plugins: [
         mkcert()
-
     ],
     server: {
         https: true
