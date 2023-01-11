1a152253c3c157ed2c524edcf0f19098ca124e01
Nandin Borjigin <dingnan@microsoft.com>
2023-01-11 16:12:27 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import mkcert from 'vite-plugin-mkcert'
 export default {
-    base: '/teams-videoapp-sample/app/',
+    base: '/teams-videoapp-sample/app/t2/',
     build: {
-        outDir: './dist/app'
+        outDir: './dist/app/t2'
     },
     plugins: [
         mkcert()
